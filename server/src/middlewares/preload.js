f4066ce5d0151428c4ffa8f369be09c2cfdc1c78
Georgi Petrov <hailanimepenchou@gmail.com>
2022-08-17 02:31:18 +0300

    server - fix delete

--- server/src/middlewares/preload.js
+++ server/src/middlewares/preload.js
@@ -1,11 +1,15 @@
 module.exports = (api) => async (req, res, next) => {
     const id = req.params.id;
 
-    const item = await api.getById(id);
-    if (item) {
-        res.locals.item = item;
-        next();
-    } else {
+    try {
+        const item = await api.getById(id);
+        if (item) {
+            res.locals.item = item;
+            next();
+        } else {
+            res.status(404).json({ message: `Item ${id} not found` });
+        }
+    } catch (err) { 
         res.status(404).json({ message: `Item ${id} not found` });
     }
 };
