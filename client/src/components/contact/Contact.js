e8b5024f22c799861526d25cd057b14db74659fe
Georgi Petrov <hailanimepenchou@gmail.com>
2022-08-20 22:05:16 +0300

    remake contact page

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -13,48 +13,7 @@ export const Contact = () => {
             <div className="contact-from-section mt-150 mb-150">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-8 mb-5 mb-lg-0">
-                            <div className="form-title">
-                                <h2>Have you got any question?</h2>
-                                <p>
-                                    Send us an email with the form below. One of our assistants will try to contact you by mail as soon as possible.
-                                </p>
-                            </div>
-                            <div id="form_status" />
-                            <div className="contact-form">
-                                <form type="POST" id="fruitkha-contact">
-                                    <p>
-                                        <input type="text" placeholder="Name" name="name" id="name" />
-                                        <input
-                                            type="email"
-                                            placeholder="Email"
-                                            name="email"
-                                            id="email"
-                                        />
-                                    </p>
-                                    <p>
-                                        <input type="tel" placeholder="Phone" name="phone" id="phone" />
-                                        <input
-                                            type="text"
-                                            placeholder="Subject"
-                                            name="subject"
-                                            id="subject"
-                                        />
-                                    </p>
-                                    <p>
-                                        <textarea
-                                            name="message"
-                                            id="message"
-                                            placeholder="Message"
-                                        />
-                                    </p>
-                                    <p>
-                                        <input type="submit" value="Send email" />
-                                    </p>
-                                </form>
-                            </div>
-                        </div>
-                        <div className="col-lg-4">
+                        <div className="col-lg-12">
                             <div className="contact-form-wrap">
                                 <div className="contact-form-box">
                                     <h4>
@@ -70,7 +29,8 @@ export const Contact = () => {
                                         <i className="fas fa-address-book" /> Contact
                                     </h4>
                                     <p>
-                                        Phone: +359 89 444 5555 <br /> Email: support@gaming.com
+                                        Phone: <a href="tel:+359894445555"> +359 89 444 5555</a> <br />
+                                        Email: <a href="mailto:support@gaming.com">support@gaming.com</a>
                                     </p>
                                 </div>
                             </div>
